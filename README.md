4e0031465df29d0d2e2631f215f1a934ad7d6594
Bernd Porr <mail@berndporr.me.uk>
2022-02-15 14:35:24 +0000

    Now shows the preview and the output

--- README.md
+++ README.md
@@ -1,7 +1,10 @@
 # attys-plot
 
-Work in progress. Do not use.
+Plots the tsv files from attys-scope.
+
+Work in progress. Not ready to be used.
 
 ## Python requirements
 
 pip install PyQtChart
+pip install plotly
--- plot_demo_data.py
+++ plot_demo_data.py
@@ -1,61 +1,104 @@
 import numpy as np
 from PyQt5.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QWidget, QHBoxLayout, QPushButton, QCheckBox
-from PyQt5.QtChart import QChart, QChartView, QLineSeries
+from PyQt5.QtChart import QChart, QChartView, QLineSeries, QValueAxis
 import sys
+from plotly.subplots import make_subplots
+import plotly.graph_objects as go
+
 
 class Window(QMainWindow):
-    def __init__(self):
+
+    channels = [
+        {"name":"ADC 1","unit":"V","idx":7,"min":-2,"max":2},
+        {"name":"ADC 2","unit":"V","idx":8,"min":-2,"max":2},
+        {"name":"Acc X","unit":"m/s^2","idx":1,"min":-10,"max":10},
+        {"name":"Acc Y","unit":"m/s^2","idx":2,"min":-10,"max":10},
+        {"name":"Acc Z","unit":"m/s^2","idx":3,"min":-10,"max":10}
+    ]
+    
+    def __init__(self, filename):
         super().__init__()
         self.setGeometry(100, 100, 680, 500)
-
-        data = np.loadtxt('triangle.tsv');
-        t = data[:,0]
-        ch1 = data[:,7]
-        ch2 = data[:,8]
-        
-        view1 = self.create_linechart(t,ch1)
-        view2 = self.create_linechart(t,ch2)
 
         central_widget = QWidget()
         self.setCentralWidget(central_widget)
         toolbar = QHBoxLayout()
         
-        createPlot = QPushButton("create plot")
+        createPlot = QPushButton("Create plot page")
         createPlot.clicked.connect(self.doPlot)
         toolbar.addWidget(createPlot)
         
         lay = QVBoxLayout(central_widget)
         lay.addLayout(toolbar)
 
-        lay1 = QHBoxLayout()
-        lay1.addWidget(view1, stretch=1)
-        cb1 = QCheckBox("Show")
-        lay1.addWidget(cb1)
-        lay.addLayout(lay1)
+        self.data = np.loadtxt(filename);
+        self.t = self.data[:,0]
 
-        lay2 = QHBoxLayout()
-        lay2.addWidget(view2, stretch=1)
-        cb2 = QCheckBox("Show")
-        lay2.addWidget(cb2)
-        lay.addLayout(lay2)
+        self.doUseCheckboxes = []
+
+        for channel in self.channels:
+            ch = self.data[:,channel['idx']]
+            view = self.createGraph(self.t,ch,channel)
+            layh = QHBoxLayout()
+            layh.addWidget(view, stretch=1)
+            cb = QCheckBox("Use")
+            cb.setChecked(True)
+            self.doUseCheckboxes.append(cb)
+            layh.addWidget(cb)
+            lay.addLayout(layh)
 
     def doPlot(self):
-        print("Plot")
+        r = 0
+        for cb in self.doUseCheckboxes:
+            if cb.isChecked():
+                r = r + 1
+        fig = make_subplots(rows=r, cols=1,
+                            x_title="t/sec",
+                            shared_xaxes=True,
+                            vertical_spacing=0.02)
 
-    def create_linechart(self,t,data):
+        r = 0
+        for channel,cb in zip(self.channels,self.doUseCheckboxes):
+            if cb.isChecked():
+                r = r + 1
+                fig.add_trace(go.Scatter(name=channel['name'],
+                                         x=self.t,
+                                         y=self.data[:,channel['idx']]),
+                              row=r, col=1)
+                fig.update_yaxes(title_text=(channel['name']+"/"+channel['unit']), row=r, col=1)
+
+        if r == 0:
+            return
+        
+        fig.update_layout(title_text="Attys data")
+        fig.show()
+
+
+    def createGraph(self,t,data,channel):
         series = QLineSeries()
         for i,j in zip(t,data):
             series.append(i,j)
 
         chart = QChart()
+        chart.legend().hide()
         chart.addSeries(series)
-        chart.createDefaultAxes()
+        axisY = QValueAxis();
+        axisY.setTitleText(channel['name']+"/"+channel['unit'])
+        axisY.setRange(channel['min'],channel['max'])
+        chart.setAxisY(axisY)
+        axisX = QValueAxis();
+        axisX.setTitleText('t/sec')
+        axisX.setRange(t[0],t[-1])
+        chart.setAxisX(axisX)
         chartview = QChartView(chart)
         return chartview
 
 
 if __name__ == "__main__":
+    filename = "triangle.tsv"
+    if len(sys.argv) > 1:
+        filename = sys.argv[1]
     App = QApplication(sys.argv)
-    window = Window()
+    window = Window(filename)
     window.show()
     sys.exit(App.exec_())
